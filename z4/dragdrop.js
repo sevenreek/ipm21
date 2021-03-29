0a31bd43c0eace71ff1fe65c4025249866eca5a9
Wojciech Walewski <w.walewski112@gmail.com>
2021-03-29 10:39:25 +0200

    adds tab functionality

--- z4/dragdrop.js
+++ z4/dragdrop.js
@@ -3,6 +3,7 @@ let id;
 let index;
 let indexDrop;
 let list;
+const TAB_KEY = 9;
 
   document.addEventListener("dragstart", ({target}) => {
       dragged = target;
@@ -20,14 +21,13 @@ let list;
   });
 
   document.addEventListener("drop", ({target}) => {
-   if(target.className == "dropzone" && target.id !== id) {
-       dragged.remove( dragged );
+   if(target.parentNode.className == "editable-list" && target.id !== id) {
+      dragged.remove( dragged );
       for(let i = 0; i < list.length; i += 1) {
         if(list[i] === target){
           indexDrop = i;
         }
       }
-      console.log(index, indexDrop);
       if(index > indexDrop) {
         target.before( dragged );
       } else {
@@ -36,27 +36,49 @@ let list;
     }
   });
 
-editor.addEventListener('keydown', (e) => {
-  var code = e.keyCode || e.which;
-  if (code == TAB_KEY) {
-    e.preventDefault(); // Stop treating it like an actual tab
-    let parent = e.target;
-    let ul = document.createElement('ul');
-    let li = document.createElement('li');
-    ul.appendChild(li);
-    parent.appendChild(ul);
-    moveCursorToEnd(li);
-  } else if (code == ENTER_KEY) {
-    e.preventDefault(); // Stop treating it like an actual line feed
-    let parent = e.target;
-    let li = document.createElement('li');
-    parent.appendChild(li);
-    moveCursorToEnd(li);
-  }
-});
+function insertAfter(newNode, referenceNode) {
+  referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+}
 
 function moveCursorToEnd(el) {
   el.focus();
   document.execCommand('selectAll', false, null);
   document.getSelection().collapseToEnd();
 }
+function addTabListener(el) {
+  el.addEventListener('keydown', (e) => {
+    var code = e.keyCode || e.which;
+    if (code == TAB_KEY) {
+      console.log("Tab pressed", e.target);
+      e.preventDefault(); // Stop treating it like an actual tab
+      let parent = e.target;
+      let ol = document.createElement('ol');
+      ol.classList.add("editable-list");
+      let li = document.createElement('li');
+      ol.appendChild(li);
+
+      var range = window.getSelection().getRangeAt(0); 
+      /*
+      // if the range is in #myDiv ;)
+      if(range.startContainer.parentNode.id==='myDiv') {
+        // delete whatever is on the range
+        range.deleteContents();
+        // place your span
+        range.insertNode(newElement);
+      }*/
+      console.log(range.commonAncestorContainer);
+      range.deleteContents();
+      insertAfter(ol, range.commonAncestorContainer);
+      //parent.appendChild(ol);
+      //moveCursorToEnd(li);
+    }
+  });
+}
+
+window.addEventListener('DOMContentLoaded', (event) => {
+
+
+  var lists = document.querySelector('.editable-list-container');
+  addTabListener(lists);
+  
+});
