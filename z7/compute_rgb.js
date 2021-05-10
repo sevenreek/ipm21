336a0a775e5ccd439816ad07ee530cd1cfbf73a7
Wojciech Walewski <w.walewski112@gmail.com>
2021-05-10 10:28:00 +0200

    Implements color change via css variable

--- z7/compute_rgb.js
+++ z7/compute_rgb.js
@@ -2,11 +2,13 @@ onmessage = function(e) {
     console.log('Message received from main script');
     var gigaSum = 0;
     var customer = e.data;
+    var gigaString = '';
     for (var key in customer) {
+        gigaString += getStringWithOnlyASCIILetters(customer[key]);
         gigaSum += sumLetters(customer[key]);
     }
 
-    console.log('Posting message back to main script');
+    console.log(gigaString);
     postMessage({
         'r': gigaSum % 255,
         'g': 255 - (gigaSum % 255),
@@ -28,5 +30,21 @@ function sumLetters(str) {
             sum += code - 96;
         }
     }
+    return sum;
 
+}
+
+function getStringWithOnlyASCIILetters(str) {
+    ret = '';
+    for (i = 0; i < str.length; i++) {
+        var code = str.charCodeAt(i);
+        if (code >= 65 && code <= 90) // uppercase
+        {
+            ret += str[i];
+        } else if (code >= 97 && code <= 122) // lowercase
+        {
+            ret += str[i];
+        }
+    }
+    return ret;
 }
--- z7/web_workers.js
+++ z7/web_workers.js
@@ -2,7 +2,6 @@ window.addEventListener('DOMContentLoaded', (event) => {
     var inverterWorker = new Worker('invert_letter.js');
     inverterWorker.onmessage = function(e) {
         putCustomerToForm(e.data);
-
     }
     var invertButton = document.getElementById('customer-invert');
 
@@ -11,11 +10,28 @@ window.addEventListener('DOMContentLoaded', (event) => {
         inverterWorker.postMessage(getCustomerJSONFromForm());
     });
 
+
+
+
+    var imageFilterWorker = new Worker('compute_rgb.js');
+    imageFilterWorker.onmessage = function(e) {
+        console.log("set color to ", e.data);
+        document.getElementById('left-image-container').style.setProperty('--image-filter-color', `rgb(${e.data.r}, ${e.data.g}, ${e.data.b})`);
+    }
     var urlElement = document.getElementById('image-url');
     urlElement.addEventListener('input', function(e) {
         console.log("Should change");
-        document.getElementById('left-image-container').style.backgroundImage = urlElement.value;
+        document.getElementById('left-image-container').style.backgroundImage = `url(${urlElement.value})`;
     });
+    var form = document.getElementById("customer-edit-form");
+    // react to form changes
+    form.addEventListener("input", function() {
+        var data = getCustomerJSONFromForm();
+        data.url = document.getElementById("image-url").value;
+        imageFilterWorker.postMessage(data);
+        console.log("Form has changed!");
+    });
+
 });
 
 
